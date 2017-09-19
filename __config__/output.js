ebd02e76decd4a9221ef5a3043935b5875dbbf1f
郭励之 <guolz@jjmmw.com>
2017-09-19 17:42:31 +0800

    修改output路径

--- __config__/output.js
+++ __config__/output.js
@@ -3,5 +3,5 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
     filename: isProd ? 'js/[name].[chunkhash].js' :'js/[name].js',
-    path: path.resolve(__dirname, './build')
+    path: path.resolve(__dirname, '../build')
 }
